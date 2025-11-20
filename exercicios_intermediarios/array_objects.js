77c8dae15176a936a6d3892ef48866207dc49c55
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-11-20 13:01:17 -0300

    .

---
+++ exercicios_intermediarios/array_objects.js
@@ -0,0 +1,29 @@
+let produtos = [
+    { nome: "Camiseta", preco: 30 },
+    { nome: "Tênis", preco: 120 },
+    { nome: "Livro", preco: 60 },
+    { nome: "Boné", preco: 25 }
+];
+
+let x = produtos.filter((p) => {
+    return p.preco > 50
+})
+
+console.log(x)
+
+// Contar quantas vezes cada número aparece
+
+let numeros = [1, 2, 2, 3, 3, 3, 4, 5, 5];
+
+let contador = {}
+
+numeros.forEach(num => {
+    if(contador[num] == undefined) {
+        contador[num] = 1
+    }
+    else {
+        contador[num]++
+    }
+})
+
+console.log(contador)
--- gerador_de_qrcode/scripts.js
+++ gerador_de_qrcode/scripts.js
@@ -1,6 +1,6 @@
 function gerar() {
-    let valor = document.querySelector("#qrcode").value
-    let chamadaAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=500x500&chld=H&chl='
+    let valor = document.getElementById("qrcode").value
+    let chamadaAPI = `http://api.qrserver.com/v1/create-qr-code/?data=${valor}!&size=100x100`
     let soma = chamadaAPI + encodeURIComponent(valor)
     let imagem = document.getElementById("image")
     imagem.src = soma
