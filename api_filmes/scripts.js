83be3ec9755ecc191c28875bae7e7cc0a2e98b13
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-12-04 12:07:05 -0300

    .

--- api_filmes/scripts.js
+++ api_filmes/scripts.js
@@ -1,14 +1,36 @@
-let movieName = "Titanic"
+async function find() {
+    let inputValue = document.getElementById("text").value
 
-async function getMovie(nome) {
+    let divValue = document.querySelector(".movies")
+
+    if (!inputValue.trim()) {
+        alert("Digite algo")
+        return
+    }
+
     try {
-        const data = await fetch(`https://www.omdbapi.com/?apikey=4a3b711b&t=${nome}`)
+        const data = await fetch(`https://www.omdbapi.com/?apikey=4a3b711b&t=${inputValue}`)
 
         const get = await data.json()
-        console.log(get)
+
+        if (get.response === "False") {
+            divValue.innerHTML = "<p>Movie not found</p>"
+            return
+        }
+
+        divValue.innerHTML = ` 
+        <div class="movie-box">
+        <img src="${get.Poster}" width="200">
+            <div>
+                <h2>${get.Title}</h2>
+                <p>Year: ${get.Year}</p>
+                <p>IMDB: ${get.imdbRating}</p>
+                <p>Actors: ${get.Actors}</p>
+            </div>
+        </div>
+        `
+        divValue.style.display = "flex"
     } catch (error) {
-        console.log(error)
+        alert(error)
     }
 }
-
-getMovie(movieName)
