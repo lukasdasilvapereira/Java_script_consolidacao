23b475bbacc2a6bdcadeb3857407f01105046b05
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-12-16 11:49:46 -0300

    Update alunos.js

--- alunos.js
+++ alunos.js
@@ -70,3 +70,62 @@ function maiorMedia(m) {
 
 let maiormedia = maiorMedia(alunos)
 console.log(maiormedia)
+
+function mediaGeral(m) {
+    let somaTotal = 0
+    let totalNotas = 0
+
+    m.forEach(element => {
+        element.notas.forEach(nota => {
+            somaTotal += nota
+            totalNotas++
+        })
+    })
+    return Number((somaTotal / totalNotas).toFixed(2))
+}
+
+let geralMedia = mediaGeral(alunos)
+console.log(geralMedia)
+
+function alunosAprovados(a) {
+    let alunosApro = []
+    let mediaFinal = 7
+
+    a.forEach(element => {
+        const nota = element.notas
+        const alunos = element.nome
+        let media = 0
+
+        let rdc = nota.reduce((valor, total) => {
+            return valor + total
+        }, 0)
+
+        media = (rdc / 3)
+        let resultado = Number(media.toFixed(2))
+
+        if (resultado >= mediaFinal) {
+            alunosApro.push(alunos)
+        }
+    });
+
+    return alunosApro
+}
+
+let aprovadosAlunos = alunosAprovados(alunos)
+console.log(aprovadosAlunos)
+
+function notaMaisAlta(n) {
+    let maiorNota = 0
+    n.forEach(element => {
+        element.notas.forEach(nota => {
+           if(nota > maiorNota){
+            maiorNota = nota
+           } 
+        });
+    })
+
+    return maiorNota
+}
+
+let maior = notaMaisAlta(alunos)
+console.log(maior)
